5ec4fce7cf164a30fc32fdfa379b83a16cde40f6
JeanExtreme002 <jeangamerextreme@gmail.com>
2024-07-14 19:01:19 -0300

    Fixed suggestions box size

--- front/src/components/SearchBox.jsx
+++ front/src/components/SearchBox.jsx
@@ -3,6 +3,8 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
+import './SearchBox.css';
+
 async function getSuggestions(text) {
   const baseURL = process.env.REACT_APP_API_URL;
 
@@ -21,6 +23,16 @@ async function getSuggestions(text) {
     });
   }
 }
+
+const ListboxComponent = React.forwardRef((props,ref) => {
+  const { children, ...other } = props;
+
+  delete other.className;
+
+  return (
+    <ul className="Autocomplete-listbox" {...other} ref={ref}>{children}</ul>
+  );
+});
 
 export default function SearchBox() {
   const [options, setOptions] = React.useState([]);
@@ -45,21 +57,17 @@ export default function SearchBox() {
 
       renderOption={(props, option) => {
         const term = option.term.replace(option.match, "<b>" + option.match + "</b>").replaceAll(" ", "&nbsp;");
-        return (<li {...props} dangerouslySetInnerHTML={{ __html: term }}></li>)
+
+        delete props.className;
+
+        return (<li className="Autocomplete-option" {...props} key={option.term} dangerouslySetInnerHTML={{ __html: term }}></li>)
       }}
 
       renderInput={(params) => (
         <TextField {...params} InputProps={{...params.InputProps, style: {height: "7vh", fontSize: "1.4vmax"}}}/>
       )}
 
-      ListboxProps={{
-        style: {
-          maxHeight: "min(20vmax, 300px)",
-          border: "1px solid",
-          whiteSpace: "nowrap",
-          fontSize: "min(1vmax, 12px)"
-        }
-      }}
+      ListboxComponent={ListboxComponent}
     />
   );
 }
